9e5e0f1dc704ed3bb8beb3b39b1c581cf2d15e8b
Acebulf <acebulf@gmail.com>
2013-10-19 12:04:28 -0300

    Populate

---
+++ FileLoader.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication5
+{
+    public class FileLoader
+    {
+        public string ice_folder = @"\textures\ice\";
+        public string jersey_folder = @"\textures\jerseys\";
+        public string true_folder = @"\data\model\";
+        public string curDirName;
+
+        public Dictionary<string, string> Dict_ice;
+        public Dictionary<string, string> Dict_jersey;
+
+        public FileLoader()
+        {
+
+            Dict_ice = new Dictionary<string, string>();
+            Dict_jersey = new Dictionary<string, string>();
+
+            curDirName = System.IO.Directory.GetCurrentDirectory();
+
+            string[] ices = System.IO.Directory.GetDirectories(curDirName + ice_folder);
+            string[] jerseys = System.IO.Directory.GetDirectories(curDirName + jersey_folder);
+
+
+            foreach (string s in ices)
+            {
+                Dict_ice.Add(s.Replace(System.IO.Path.GetDirectoryName(s) +"\\", ""), s);
+            }
+
+            foreach (string s in jerseys)
+            {
+                Dict_jersey.Add(s.Replace(System.IO.Path.GetDirectoryName(s) + "\\", ""), s);
+            }
+        }
+
+        public List<string> returnJerseyList()
+        {
+            return Dict_jersey.Keys.ToList<string>();
+        }
+
+        public List<string> returnIceList()
+        {
+            return Dict_ice.Keys.ToList<string>();
+        }
+
+        public void set_ice(string name)
+        {
+            Debug.Print(name);
+            string icefolder = Dict_ice[name];
+
+            foreach (string s in System.IO.Directory.GetFiles(icefolder))
+            {
+                System.IO.File.Copy(s, curDirName + true_folder + s.Replace(icefolder, ""), true);
+            }
+
+
+        }
+
+        public void set_blue(string name)
+        {
+            string bluefolder = Dict_jersey[name] + @"\blue\";
+
+            foreach (string s in System.IO.Directory.GetFiles(bluefolder))
+            {
+                System.IO.File.Copy(s, curDirName + true_folder + s.Replace(bluefolder, ""), true);
+            }
+        }
+
+        public void set_red(string name)
+        {
+            string redfolder = Dict_jersey[name] + @"\red\";
+
+            foreach (string s in System.IO.Directory.GetFiles(redfolder))
+            {
+                System.IO.File.Copy(s, curDirName + true_folder + s.Replace(redfolder, ""), true);
+            }
+
+        }
+
+        public void launch()
+        {
+            string swag = curDirName + @"\hockey.exe";
+            System.Diagnostics.Process.Start(curDirName + @"\hockey.exe"); 
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        public FileLoader fileloader;
+        public string cmb1;
+        public string cmb2;
+        public string cmb3;
+        public Form1()
+        {
+            fileloader = new FileLoader();
+            InitializeComponent();
+            foreach (string s in fileloader.returnJerseyList())
+            {
+                this.comboBox1.Items.Add(s);
+                this.comboBox2.Items.Add(s);
+            }
+
+            foreach (string s in fileloader.returnIceList())
+            {
+                this.comboBox3.Items.Add(s);
+            }
+
+        }
+        
+        public void Commit(){
+            fileloader.set_ice(cmb3);
+            fileloader.set_red(cmb1);
+            fileloader.set_blue(cmb2);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Red team
+            cmb1 = this.comboBox1.SelectedItem.ToString();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Blue team
+            cmb2 = this.comboBox2.SelectedItem.ToString();
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Ice
+            cmb3 = this.comboBox3.SelectedItem.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Save changes
+            Commit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Save and open hockey?
+            Commit();
+            fileloader.launch();
+            this.Close();
+        }
+    }
+}
